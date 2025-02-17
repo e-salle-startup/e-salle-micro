b00cbc96ed4036cfa8e2e34b7a8f9fc8287aa698
Anas Aliky <anasalk37@gmail.com>
2025-02-17 15:13:56 +0100

    first-commit

---
+++ src/main/java/org/anas/esallemicro/ESalleMicroApplication.java
@@ -0,0 +1,13 @@
+package org.anas.esallemicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ESalleMicroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ESalleMicroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/anas/esallemicro/ESalleMicroApplicationTests.java
@@ -0,0 +1,13 @@
+package org.anas.esallemicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ESalleMicroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
